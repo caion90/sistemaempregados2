b2bdfce7675444a6bd6248dcfcfa524f2228d0fd
caion90 <105600461+caion90@users.noreply.github.com>
2022-05-26 14:30:00 -0300

    Update Navbar.jsx

--- react-frontend/src/components/Navbar.jsx
+++ react-frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className='bg-green-800'>
+        <div className='bg-slate-800'>
             <div className='h-16 px-8 flex items-center'>
                 <p className='text-white font-bold'>Sistema de controle</p>
             </div>
